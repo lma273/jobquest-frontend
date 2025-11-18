f1a8bb49283ec2a0dda84615fd127ef738414480
lma273 <23020510@vnu.edu.vn>
2025-11-18 11:23:52 +0700

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080", // tuỳ bạn
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Gắn token Bearer từ localStorage
