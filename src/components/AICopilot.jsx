7a2c9e7974709a3ac4dd297f00bc8a98654f79e9
hant0505 <23020532@vnu.edu.vn>
2025-12-18 13:17:27 +0700

    ngrok

--- src/components/AICopilot.jsx
+++ src/components/AICopilot.jsx
@@ -206,7 +206,7 @@ const AICopilot = ({ selectedJob }) => {
       };
 
       // Gọi API Backend
-      const response = await fetch("http://127.0.0.1:8000/consult", {
+      const response = await fetch("https://lakeisha-unhumorous-histographically.ngrok-free.dev/consult", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
